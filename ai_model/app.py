3d7064b881a7e77bad393ae290d2e2436c7ad185
Guilherme Pereira <guilherme.borges@xplanning.com.br>
2024-09-15 19:00:08 -0300

    2024-09-15 - Ajuste no modelo

--- ai_model/app.py
+++ ai_model/app.py
@@ -8,8 +8,13 @@ app = FastAPI()
 
 @app.get('/stock_market/{id_stock_market}')
 async def stock_market(id_stock_market:str):
-    previsoes = model.stock_market_model.predict(model.future)
-    return {
-        'ticker': id_stock_market,
-        'forecast': previsoes['yhat'][0]
-    }
+    if( id_stock_market == 'WEGE' ):
+        previsoes = model.stock_market_model.predict(model.future)
+        return {
+            'ticker': id_stock_market,
+            'forecast': previsoes['yhat']
+        }
+    else:
+        return {
+            'error' : 'Ticker not found'
+        }
--- ai_model/model.py
+++ ai_model/model.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import yfinance as yf
+import pandas as pd # type: ignore
+import yfinance as yf # type: ignore
 
-from prophet import Prophet
-from singleton_decorator import singleton
+from prophet import Prophet # type: ignore
+from singleton_decorator import singleton # type: ignore
 
 @singleton
 class Model:
@@ -11,16 +11,14 @@ class Model:
     future = None
 
     def initialize(self):
-        #acao_df = pd.DataFrame()
-        #acao_df['WEGE3.SA'] = yf.download(tickers='WEGE3.SA', period='5Y')['Close']
-        #acao_df = acao_df.rename(columns={'WEGE3.SA': 'WEGE'})
-        acao_df = pd.read_csv('acao.csv', usecols = ['Date', 'WEGE'])
-        acao_df.dropna(inplace=True)
+        acao_df = yf.download(tickers='WEGE3.SA', period='2Y')
         self.stock_market_model = self.prepare(acao_df)
         self.future = self.stock_market_model.make_future_dataframe(periods=90)
 
     def prepare(self, df):
-        dataset = df[['Date', 'WEGE']].rename(columns = {'Date': 'ds', 'WEGE': 'y'})
-        modelo = Prophet(interval_width=0.9, changepoint_prior_scale=0.2)
-        modelo.fit(dataset)
-        return modelo
+        target_df = df['Close']
+        target_df = target_df.reset_index('Date')
+        target_df.columns = ['ds', 'y']
+        model = Prophet(interval_width=0.9, changepoint_prior_scale=0.2)
+        model.fit(target_df)
+        return model
