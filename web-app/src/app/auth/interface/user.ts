e7ad45e44e942d24d68906c09b384f654c190604
Guilherme Pereira <guilherme.borges@xplanning.com.br>
2024-11-26 20:44:26 -0300

    Chore: Remoção do projeto web-app

--- web-app/src/app/auth/interface/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    token: string,
-    message: string,
-    password: string,
-    userName: string,
-}
--- web-app/src/app/interface/transaction.ts
+++
@@ -1,8 +0,0 @@
-export interface Transaction {
-    id: string;
-    situacao: string;
-    data: string;
-    descricao: string;
-    categoria: string;
-    valor: string;
-}
